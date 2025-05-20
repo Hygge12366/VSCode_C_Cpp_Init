e35523ed73188befcf1fc91cd1f9753364665b4b
Hygge12366 <131175193+Hygge12366@users.noreply.github.com>
2025-05-20 14:47:41 +0800

    Update hello.c

--- C/demo01/hello/hello.c
+++ C/demo01/hello/hello.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-	printf("Hello World!\n"); //f 表示formatted 格式化的
+	printf("Hello World!\n");
 	
-	printf("%d\n", 12 + 34); //%d 说明后面有一个整数要输出在这个位置上
+	printf("%d\n", 12 + 34);
 	printf("23+43=%d\n", 23 + 43);
 	
 	return 0;
